e8fa0ef530ebfd518de0a78aefcd7823dce3a7d4
a455bcd9 <a455bcd9@users.noreply.github.com>
2014-05-03 00:13:33 +0200

    Update 16LF1787 brouillon.c

--- PIC/16LF1787 brouillon.c
+++ PIC/16LF1787 brouillon.c
@@ -1,6 +1,7 @@
-// http://www.egr.msu.edu/classes/ece480/capstone/spring12/group03/Documents/Ryan_ApplicationNote.pdf
-// http://electrosome.com/adc-pic-microcontroller-hi-tech-c/
-// http://www.technoburst.net/2013/04/pic16f877a-adc-example.html
+// Configuration de la PIClF1787
+// Selection d une capacite a l aide des multiplexeurs ligne / colonne
+// Fonction ADC : lecture de la tension de sortie
+// Envoi des valeurs au port serie ordinateur
 
 // Definitions
 #define _XTAL_FREQ  32000000    // this is used by the __delay_ms(xx) and __delay_us(xx) functions
@@ -8,14 +9,14 @@
 
 // 'C' source line config statements
 #include <xc.h>
-//#include <pic16lf1787.h>
+// #include <pic16lf1787.h> Inutile deja dans pic.h dans htc.h dans xc.h
 
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 
 // CONFIGURATION BITS p 53 datasheet
 #pragma config FOSC = INTOSC    // Oscillator Selection bits (INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, I/O function  on RA7/OSC1/CLKIN)
-								// frequence interne de 32Mhz
+				// frequence interne de 32Mhz
 #pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
 #pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
 #pragma config MCLRE = ON       // RE3/MCLR Pin Function Select bit (RE3/MCLR pin function is MCLR)
@@ -29,36 +30,31 @@ unsigned short ADC_Read()
 {
 	unsigned short ADCResult ;
 
-// while(1)
-//   {
 	__delay_us(1); //Waits for the acquisition to complete 1 microseconde
 	ADCON0bits.GO = 1;   //Starts ADC conversion
 	while(ADCON0bits.DONE);   //wait till ADC conversion is over
 	ADCResult = (ADRESH<<8) + ADRESL ;   //Merging the MSB and LSB
-// ADRESH<<8 ou bien ADRESHbits.AD ?
+	// ADRESH<<8 ou bien ADRESHbits.AD ?
    return ADCResult ;
-//   }
 }
 
 void main()
 {
-// unsigned int a;
-    // set up oscillator control register for internal clock running at 32hz
-	OSCCON = 0xF8;	// 11111000 On demande 32 Mhz le resultat depend de PLLEN cf p 56 + 74 + 82
+	// set up oscillator control register for internal clock running at 32hz
+	OSCCON = 0xF8;	// 11111000 On demande 32 Mhz le resultat depend de PLLEN cf pp 56 + 74 + 82
 
-//	OPTION_REGbits.nWPUEN = 0; // enable weak pullups (each pin must be enabled individually)
+	// OPTION_REGbits.nWPUEN = 0; // enable weak pullups (each pin must be enabled individually)
 
-    // PORT A Assignments
+	// PORT A Assignments
 	TRISA = 0xFF; //PORTA as input RA0
 
 	ANSELAbits.ANSA0 = 1; // Pin is assigned as analog input. Digital input buffer disabled. Analog Select between Analog or Digital Function on Pins RA0
 
-// for( ; (OSCCON & 0xC) != 0xC ; );  On attend la stabilisation de l'oscillateur pour continuer
-// ANSELA = 2; // initialise les registres ANSELA et ANSELB  0  I/O numérique ;
-// 1 : input ; 0 : Output
-// ANSELB = 0;
+	// for( ; (OSCCON & 0xC) != 0xC ; );  On attend la stabilisation de l'oscillateur pour continuer
+	// ANSELA = 2; // initialise les registres ANSELA et ANSELB  0  I/O numérique ;
+	// 1 : input ; 0 : Output
 
-    // PORT B Assignments
+	// PORT B Assignments
 	TRISB0 = 0; // RB0 as output VDD
 	TRISB1 = 0; // nc
 	TRISB2 = 0; // nc
@@ -70,7 +66,7 @@ void main()
 
 	ANSELB = 0x00; // all port B pins are digital I/O
 
-    // PORT C Assignments
+	// PORT C Assignments
 	TRISC0 = 1; // multiplexeur en sortie
 	TRISC1 = 1; // idem
 	TRISC2 = 1; // idem
@@ -84,27 +80,27 @@ void main()
         APFCON1bits.RXSEL = 0; // page 131 RX is on pin RC7
 
 
-//	ANSELC = ANSELC & 0x3D; // RC6 = TXREG et RC7 = RXREG pas sur PIC16LF1787
+	// ANSELC = ANSELC & 0x3D; // RC6 = TXREG et RC7 = RXREG pas sur PIC16LF1787
 
-    //	PORT D Assignments
+	// PORT D Assignments
 	TRISD = 0x00; 	// multiplexeur en sortie
 
 	ANSELD = 0x00;    // all port D pins are digital I/O
 
-    //	PORT E Assignments
+	// PORT E Assignments
 	TRISE = 0xFF; 	// RE3 MCLR as input
 
 	ANSELE = 0x00;    // all port E pins are digital I/O
 
 	//**********************************************************************************
-    // Set up the ADC
-    //**********************************************************************************
-    // Note that you can choose from different sources for the ADC positive voltage source.
-    // Choosing Vdd can get you a wider output range, but choosing the
-    // FVR (Fixed Voltage Reference) will typically get you a much
-    // more stable reference voltage than Vdd. The FVR can also be used as the Vref for
-    // the DAC.  Read the section in the datasheet about the FVR for more details.
-    //**********************************************************************************
+	// Set up the ADC
+	//**********************************************************************************
+	// Note that you can choose from different sources for the ADC positive voltage source.
+	// Choosing Vdd can get you a wider output range, but choosing the
+	// FVR (Fixed Voltage Reference) will typically get you a much
+	// more stable reference voltage than Vdd. The FVR can also be used as the Vref for
+	// the DAC.  Read the section in the datasheet about the FVR for more details.
+	//**********************************************************************************
 
 	ADCON1bits.ADCS = 0b010;	// Selecting the clk division factor = FOSC/32 cf page 169 ADC Clock Period TAD = 1.0 µs
 	ADCON1bits.ADFM = 1;		// Result right justified (1 = 2 s complement format 0 = Sign-magnitude result format )
@@ -116,11 +112,11 @@ void main()
 
 
 	//*************************************************************************************
-    // Set up the UART
-    // Note: See the datasheet for tables with the values of SPBRGH:SPBRGL already
-    // calculated based on different values of Fosc and BRGH and desired
-    // baud rates.
-    //*************************************************************************************
+    	// Set up the UART
+    	// Note: See the datasheet for tables with the values of SPBRGH:SPBRGL already
+    	// calculated based on different values of Fosc and BRGH and desired
+    	// baud rates.
+    	//*************************************************************************************
 
 	// page 359 8 bit asynchronous SYNC = 0 BRG16 = 0 BRGH = 0
 	// BRG16 dans BAUDCON page 357
@@ -134,21 +130,21 @@ void main()
 /* 	TXSTA = TXSTA & 0xDF; 		// TX9 = 1 on transmet 9 bits (donc la non)
 	TXSTA = TXSTA | 1; 			// TX9D = 9eme bit a transmettre à 1 (=stop)
 	TXSTA = TXSTA & 0xFB; 		// BRGH = 0 low speed */
-    TXSTAbits.BRGH = 0;		// select low speed Baud Rate (see baud rate calcs below)
-    TXSTAbits.TX9  = 0;		// select 8 data bits
-    TXSTAbits.TXEN = 1;     // enable transmit
-    TXSTAbits.SYNC = 0; 	// asynchronous
+    	TXSTAbits.BRGH = 0;		// select low speed Baud Rate (see baud rate calcs below)
+    	TXSTAbits.TX9  = 0;		// select 8 data bits
+    	TXSTAbits.TXEN = 1;     // enable transmit
+    	TXSTAbits.SYNC = 0; 	// asynchronous
 
 /* 	RCSTA = RCSTA | 0x20; 		// SREN = 1 réception serie activee
 	RCSTA = RCSTA & 0xDF; 		// RX9 = 0 on n'attend que 8 bits
 	RCSTA = RCSTA | 0x04; 		// FERR=1
 	RCSTA = RCSTA | 0x02; 		// OERR=1 */
 
-    RCSTAbits.SPEN=1;       // serial port is enabled
-    RCSTAbits.RX9=0;        // select 8 data bits
-    RCSTAbits.FERR=1;       // Framing Error
-    RCSTAbits.OERR=1;       // Overrun Error
-    RCSTAbits.CREN=1;       // receive enabled
+    	RCSTAbits.SPEN=1;       // serial port is enabled
+    	RCSTAbits.RX9=0;        // select 8 data bits
+    	RCSTAbits.FERR=1;       // Framing Error
+    	RCSTAbits.OERR=1;       // Overrun Error
+    	RCSTAbits.CREN=1;       // receive enabled
 
 /* 	// Monsieur Microchip dit que ces 3 la doivent etre dans
 	// cet etat pour avoir une liaison serie asynchrone
@@ -161,20 +157,20 @@ void main()
 	SPBRG = 51; 				// 9600 bauds avec une horloge INTOSC = 32 MHz BRGH = 0 et BRG16 = 0 pour 8 bit asynchronous (p 360 de la doc)
 
 	PIR1bits.RCIF=0;        // make sure receive interrupt flag is clear
-    PIE1bits.RCIE=1;        // enable UART Receive interrupt
-    INTCONbits.PEIE = 1;    // Enable peripheral interrupt
-    INTCONbits.GIE = 1;     // enable global interrupt
+    	PIE1bits.RCIE=1;        // enable UART Receive interrupt
+    	INTCONbits.PEIE = 1;    // Enable peripheral interrupt
+	INTCONbits.GIE = 1;     // enable global interrupt
 
 do
 {
-// Inserer code de Guillaume
+// Inserer code de Guillaume de selection des lignes / colonnes
 // Faire les 36 mesures
 
 /* 	a = ADC_Read(); //Reading Analog Channel (On lit déjà AN0)
 // b = éliminer les bits de poids faible de a et envoyer un octet
 	__delay_ms(50);        // give time for voltage levels on board to settle */
 
-	TXREG = 'H';
+	TXREG = 'H'; // Test de EUSART
 }while(1); //Infinite Loop
 }
 
@@ -235,6 +231,9 @@ double calcul;
 /* ------------------------------------------------
 
 // Code de Guillaume à insérer dans celui de Li
+// Erreur sur entree 1 et entree 2 a corriger
+// Inscrire les equivalences entree / ligne et sortie / colonne
+// Debrancher l entree apres la mesure puis attendre 5 tau la decharge du condo puis faire le nouveau branchement et le changement de voie
 
 	PORTDbits.RD3 = 1; //active multiplexeur droit (entree = ligne)
 	PORTCbits.RC3 = 1; //active multiplexeur gauche (sortie = colonne)
